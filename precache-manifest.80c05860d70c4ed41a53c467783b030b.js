13686ebb69adbc430367365b2a7a09e1d8f68401
Roksana Dzioba <roksanadzioba@gmail.com>
2020-01-30 21:47:02 +0100

    Updates

---
+++ precache-manifest.80c05860d70c4ed41a53c467783b030b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91d6c79038c0f8af8b8a04755b6c6b1",
+    "url": "/counter-app/index.html"
+  },
+  {
+    "revision": "743fc45b77ea8dee6144",
+    "url": "/counter-app/static/css/main.5da2aaf3.chunk.css"
+  },
+  {
+    "revision": "194c672a1a40abaa35bf",
+    "url": "/counter-app/static/js/2.18a53326.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/counter-app/static/js/2.18a53326.chunk.js.LICENSE"
+  },
+  {
+    "revision": "743fc45b77ea8dee6144",
+    "url": "/counter-app/static/js/main.7c70448a.chunk.js"
+  },
+  {
+    "revision": "8d70c5fdd48a0f742b21",
+    "url": "/counter-app/static/js/runtime-main.7b1616cb.js"
+  }
+]);
